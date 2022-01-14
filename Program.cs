90af89a696c2b48c7eabd5ce955c0995e80eac8e
harshithasvisionet <Harshitha.S@visionetsystems.com>
2022-01-14 09:19:52 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace GitHub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 10, 20, 30, 40, 50 };
+            
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine(arr[i]);
+
+            
+            }
+        }
+    }
+}
