0d0848f024640051c8677094d37f8c4e1bbbbac0
harshithasvisionet <Harshitha.S@visionetsystems.com>
2022-01-14 09:50:50 +0530

    updated console statement

--- Program.cs
+++ Program.cs
@@ -7,12 +7,13 @@ namespace GitHub
         static void Main(string[] args)
         {
             int[] arr = { 10, 20, 30, 40, 50 };
-            
+
             for (int i = 0; i < arr.Length; i++)
             {
                 Console.WriteLine(arr[i]);
+                Console.WriteLine("Is the number");
 
-            
+           
             }
         }
     }
